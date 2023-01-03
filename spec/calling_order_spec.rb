9a31cb206144d576783d3ed56393a05f7e4f7d00
Adam Bachman <abachman@recurly.com>
2023-01-03 12:10:25 -0500

    add calling_order_spec test file

---
+++ spec/calling_order_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'processor'
+
+# One test from  processor_spec with puts statements in every block.
+# Question: what order do you think they'll appear in?
+
+describe Processor do
+  before { skip }
+
+  puts 'describe Processor'
+
+  subject {
+    puts 'subject'
+    described_class.new(records: records, method: method, field: field)
+  }
+
+  let(:method) {
+    puts "let(:method)"
+    :add
+  }
+  let(:field) {
+    puts "let(:field)"
+    :value
+  }
+  let(:records) {
+    puts "let(:records)"
+    [{ value: 99 }]
+  }
+
+  describe '#do_work' do
+    puts 'describe #do_work'
+    context 'with no records' do
+      puts "context 'with no records'"
+      before do
+        puts 'before'
+        expect(subject).to receive(:values).and_return([])
+      end
+
+      it 'produces 0' do
+        puts "it 'produces 0'"
+        expect(subject.do_work).to eq(0)
+      end
+    end
+  end
+end
